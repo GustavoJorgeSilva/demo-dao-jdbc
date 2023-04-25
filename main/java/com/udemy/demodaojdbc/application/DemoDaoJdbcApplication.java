220d07d822a494bbd2fe4ed4ed09d879f49b66fa
GustaVoJorgeSilva <gustavo.jorge@hotmail.com.br>
2023-04-25 12:04:09 -0300

    Criação da classe department

--- main/java/com/udemy/demodaojdbc/application/DemoDaoJdbcApplication.java
+++ main/java/com/udemy/demodaojdbc/application/DemoDaoJdbcApplication.java
@@ -1,5 +1,6 @@
-package com.udemy.demodaojdbc;
+package com.udemy.demodaojdbc.application;
 
+import com.udemy.demodaojdbc.model.entities.Department;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,13 @@ public class DemoDaoJdbcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoDaoJdbcApplication.class, args);
-    }
 
+
+        Department department = new Department(1,"Books");
+        System.out.println(department);
+
+
+
+
+    }
 }
