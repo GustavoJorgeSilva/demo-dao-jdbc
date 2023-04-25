708e0eed17b3381b3ac127d023cda0fff47b5c7c
GustaVoJorgeSilva <gustavo.jorge@hotmail.com.br>
2023-04-25 14:14:53 -0300

    Criação das interfaces SellerDao e DepartmentDao

---
+++ main/java/com/udemy/demodaojdbc/model/dao/DepartmentDao.java
@@ -0,0 +1,13 @@
+package com.udemy.demodaojdbc.model.dao;
+
+import com.udemy.demodaojdbc.model.entities.Department;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+
+}
