ea2aad2fb8ad847427125ab4e0bd496d0572c973
GustaVoJorgeSilva <gustavo.jorge@hotmail.com.br>
2023-04-25 15:42:13 -0300

    Melhoria do metodo findById

--- main/java/com/udemy/demodaojdbc/application/DemoDaoJdbcApplication.java
+++ main/java/com/udemy/demodaojdbc/application/DemoDaoJdbcApplication.java
@@ -22,6 +22,7 @@ public class DemoDaoJdbcApplication {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== Test 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
